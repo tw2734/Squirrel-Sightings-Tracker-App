089617329f3bf864f071df7400e8858b820da30c
Tianling Wang <tw2734@columbia.edu>
2019-12-09 04:00:44 +0000

    Edit urls,admin,forms

--- project/urls.py
+++ project/urls.py
@@ -12,12 +12,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 from sightings import views
 
 urlpatterns = [
+    path('',views.home),
     path('admin/', admin.site.urls),
     path('sightings/', include('sightings.urls')),
     path('map/', views.squirrel_location),
